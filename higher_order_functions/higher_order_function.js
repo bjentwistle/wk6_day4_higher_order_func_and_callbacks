7438f1a3103b933f0869bf8fed40479730aa4726
Becky <bjentwistle@gmail.com>
2023-05-11 12:00:35 +0100

    initial commit

---
+++ higher_order_functions/higher_order_function.js
@@ -0,0 +1,62 @@
+//Functions that are passed to or returned from another function
+
+const myNumbers = [1,2,3,4,5]
+
+// for (const number of myNumbers) {
+//     console.log(number)
+// }
+
+// const logNumber = (number) => {
+//     console.log(number*2)
+// }
+// myNumbers.forEach(logNumber) //this forEach function takes in another function logNumber
+
+// myNumbers.forEach((number, index, myNumbers) => { //simplification of above with ofEach(value:number, index:number, array :number[] )
+//     console.log(`This is the number ${number} and it is at index ${index} of ${myNumbers}`)
+// })
+
+const numbers = [5,6,7,8,9]
+const multipleByTwo = (numbers) => {
+    const result = []
+    numbers.forEach((number) => {    //forEach() has no return value
+        const multipliedNumber = number*2
+        result.push(multipliedNumber)   //add number into the array of result.
+    })
+    console.log(result)
+    return result
+    
+}
+multipleByTwo(numbers)
+
+// Using the forEach method, complete the following tasks:
+// Write a function called getEvens that takes an array of numbers as an argument and returns a new array that only contains the even numbers from the original array.
+// Write a function called sumElements that takes an array of numbers as an argument and returns the sum total of all the elements in the array.
+
+const moreNumbers = [2,3,4,5,6,7,7,8,9,9,10]
+
+const getEvens = (numbers) => {
+    const results = []
+
+    numbers.forEach( (number) => {
+        if (number%2 ===0 ) {
+            results.push(number)
+        }
+    })
+    
+    return results
+};
+
+console.log(getEvens(moreNumbers))
+
+
+const sumElements = (numbers) => {
+    let runningTotal = 0
+
+    numbers.forEach((number) => {
+        runningTotal += number
+    })
+    
+    return runningTotal
+};
+
+console.log(sumElements(moreNumbers))
